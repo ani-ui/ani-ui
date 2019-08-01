c5735670961855e21e8094e8f8e8e80700deb3a6
laiiihz <laiiihz@outlook.com>
2019-08-01 17:32:47 +0800

    update docs

--- src/view/menus/MenuButton.vue
+++ src/view/menus/MenuButton.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        <h1>按钮</h1>
+        <h1>ani-button 按钮</h1>
+        <h2>基础使用</h2>
+        <ani-button>按钮</ani-button>
+        <prism-editor :code="codeButton" language="markup"></prism-editor>
     </div>
 </template>
 
 <script>
+    import "prismjs";
+    import "prismjs/themes/prism.css";
+    import PrismEditor from "vue-prism-editor";
+    import AniButton from "../../components/AniButton";
     export default {
-        name: "MenuButton"
+        name: "MenuButton",
+        components: { AniButton ,PrismEditor},
+
+        data: function () {
+            return {codeButton: '<ani-button>按钮</ani-button>'}
+        }
     }
 </script>
 
