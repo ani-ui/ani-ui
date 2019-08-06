a69607eee06023f5fb94e2ee0b06a7b2965803d0
laiiihz <laiiihz@outlook.com>
2019-08-06 14:44:13 +0800

    [update] update ani-progress

--- src/components/AniProgress.vue
+++ src/components/AniProgress.vue
@@ -1,21 +1,76 @@
 <template>
-  <div v-if="!circle" :class="[loading ? 'ani-progress-loading-style' : '']">
-    <div :class="[loading ? 'ani-progress-loading-inside' : '']"></div>
+  <div v-if="!circle" class="ani-progress-loading-style">
+    <div class="ani-progress-loading-inside" v-if="loading"></div>
+    <div :style="`width:${percent}%`" class="ani-progress-default" v-else></div>
   </div>
-  <div v-else :class="[loading ? 'ani-progress-circle-loading-style' : '']">
+  <div v-else class="ani-progress-circle-loading-style">
     <div class="circle-loading-inside-style">
       <div class="circle-loading-inside-circle"></div>
     </div>
-    <div class="loading-inside-hidden"></div>
+    <div v-if="loading" class="loading-inside-hidden"></div>
+    <div v-else style="height: 140px;width: 140px;">
+      <canvas id="canvas-circle"></canvas>
+    </div>
   </div>
 </template>
 
 <script>
+import { TweenLite, Power3 } from "gsap";
 export default {
+  mounted() {
+    if (this.circle) {
+      let canvas = document.getElementById("canvas-circle");
+      let ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.arc(
+        70,
+        70,
+        70,
+        Math.PI * 1.5,
+        ((Math.PI * 2) / 100) * this.percent + Math.PI * -0.5
+      );
+      ctx.lineTo(70, 70);
+      ctx.closePath();
+      ctx.fillStyle = "#959394";
+      ctx.fill();
+    }
+  },
   name: "AniProgress",
+  computed: {},
+  watch: {
+    animatePercent(value) {
+      let canvas = document.getElementById("canvas-circle");
+      let ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.arc(
+        70,
+        70,
+        70,
+        Math.PI * -0.5,
+        ((Math.PI * 2) / 100) * value + Math.PI * -0.5
+      );
+      ctx.lineTo(70, 70);
+      ctx.closePath();
+      ctx.fill();
+      ctx.fillStyle = "#959394";
+    },
+    percent(value) {
+      TweenLite.to(this.$data, 1, {
+        animatePercent: value,
+        ease: Power3.easeInOut
+      });
+    }
+  },
   props: {
     loading: Boolean,
-    circle: Boolean
+    circle: Boolean,
+    percent: Number
+  },
+  data() {
+    return {
+      animatePercent: this.percent
+    };
   }
 };
 </script>
@@ -46,6 +101,11 @@ export default {
   background-color: rgba(0, 0, 0, 0.4);
   animation: 3s animate-loading-progress infinite;
 }
+.ani-progress-default {
+  height: 15px;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0.4);
+}
 @keyframes animate-loading-progress {
   from {
     transform: translateX(-110%);
@@ -63,19 +123,19 @@ export default {
   background-color: white;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   overflow: hidden;
-    transition: all 500ms;
+  transition: all 500ms;
 }
 .ani-progress-circle-loading-style:hover {
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.4);
 }
 .ani-progress-circle-loading-style:active {
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
 }
 .ani-progress-circle-loading-style:hover .circle-loading-inside-style {
-    box-shadow:inset 0 0 15px rgba(0, 0, 0, 0.4);
+  box-shadow: inset 0 0 15px rgba(0, 0, 0, 0.4);
 }
-.ani-progress-circle-loading-style:active  .circle-loading-inside-style{
-    box-shadow:inset 0 0 5px rgba(0, 0, 0, 0.3);
+.ani-progress-circle-loading-style:active .circle-loading-inside-style {
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.3);
 }
 @keyframes animate-circle-loading-progress {
   from {
@@ -94,8 +154,8 @@ export default {
   z-index: 2;
   border-radius: 100px;
   background-color: white;
-    box-shadow:inset 0 0 10px rgba(0, 0, 0, 0.2);
-    transition: all 500ms;
+  box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
+  transition: all 500ms;
 }
 
 .loading-inside-hidden {
--- src/view/menus/MenuProgress.vue
+++ src/view/menus/MenuProgress.vue
@@ -2,7 +2,15 @@
     <div style="width: 90%">
         <h1>进度条</h1>
         <card-code-display>
+            <ani-progress style="margin-left: 20px" :percent="20"></ani-progress>
+        </card-code-display>
+        <card-code-display>
             <ani-progress loading style="margin-left: 20px"></ani-progress>
+        </card-code-display>
+        <card-code-display>
+            <ani-progress   circle :percent="percentage" style="margin-left: 20px"></ani-progress>
+            <ani-button @click="handleAdd">add</ani-button>
+            <ani-button @click="handleSub">sub</ani-button>
         </card-code-display>
         <card-code-display>
             <ani-progress loading circle style="margin-left: 20px"></ani-progress>
@@ -13,9 +21,23 @@
 <script>
     import AniProgress from "../../components/AniProgress";
     import CardCodeDisplay from "../component/CardCodeDisplay";
+    import AniButton from "../../components/AniButton";
     export default {
         name: "MenuProgress",
-        components: {CardCodeDisplay, AniProgress}
+        components: {CardCodeDisplay, AniProgress,AniButton},
+        data(){
+            return {
+                percentage:10,
+            }
+        },
+        methods: {
+            handleAdd() {
+                this.percentage+=10
+            },
+            handleSub() {
+                this.percentage-=10
+            }
+        }
     }
 </script>
 
