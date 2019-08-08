0a728b8cd51fc73ebaf05dc9593238a2a5787331
laiiihz <laiiihz@outlook.com>
2019-08-08 14:34:06 +0800

    [update] update bottomBar docs

--- src/view/menus/MenuBottomBar.vue
+++ src/view/menus/MenuBottomBar.vue
@@ -3,8 +3,12 @@
     <h1>ani-bottom-bar 底部导航栏</h1>
     <card-code-display>
       <ani-bottom-bar>
-          <ani-bottom-bar-item id="111D"></ani-bottom-bar-item>
-          <ani-bottom-bar-item id="222D"></ani-bottom-bar-item>
+          <ani-bottom-bar-item >
+              <i class="material-icons">home</i>
+          </ani-bottom-bar-item>
+          <ani-bottom-bar-item >
+              <i class="material-icons">face</i>
+          </ani-bottom-bar-item>
       </ani-bottom-bar>
     </card-code-display>
   </div>
