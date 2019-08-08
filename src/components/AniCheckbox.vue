e9207577d384db7524bf8cb44af08bd8b03043b6
laiiihz <laiiihz@outlook.com>
2019-08-08 10:34:36 +0800

    [update] update ani-checkbox style

--- src/components/AniCheckbox.vue
+++ src/components/AniCheckbox.vue
@@ -7,7 +7,12 @@
         :checked="checked"
         @change="handleChange"
       />
-      <div class="ani-checkbox--main-style"></div>
+      <div class="ani-checkbox--main-style">
+          <div class="ani-checkbox-checked__mark">
+              <div class="ani-checkbox-mark__left"></div>
+              <div class="ani-checkbox-mark__right"></div>
+          </div>
+      </div>
     </div>
   </label>
 </template>
@@ -35,16 +40,60 @@ export default {
 </script>
 
 <style scoped>
+    .ani-checkbox-parent-style{
+        position: relative;
+    }
 .ani-checkbox--main-style {
   width: 20px;
   height: 20px;
-    box-shadow: inset 0 0 5px rgba(0,0,0,0.3);
+    box-shadow: 0 0 0 rgba(0,0,0,0.3),inset 0 0 5px rgba(0,0,0,0.3);
   border-radius: 5px;
   border: 1px solid rgba(0, 0, 0, 0.3);
+    position: relative;
+    transition: all 500ms;
 }
+    .ani-checkbox--main-style:hover{
+        box-shadow:  0 0 10px rgba(0,0,0,0.3) , inset 0 0 5px rgba(0,0,0,0.5);
+    }
+    .ani-checkbox--main-style:active{
+        box-shadow:  0 0 5px rgba(0,0,0,0.3) , inset 0 0 2px rgba(0,0,0,0.5);
+    }
 .ani-checkbox-parent-style
   input[type="checkbox"]:checked
   + .ani-checkbox--main-style {
-  background-color: pink;
+  background-color: rgba(0,0,0,0.4);
 }
+    .ani-checkbox-parent-style
+    input[type="checkbox"]:checked
+    + .ani-checkbox--main-style .ani-checkbox-checked__mark{
+        transform: rotate(0) scale(1);
+        opacity: 1;
+    }
+    .ani-checkbox-checked__mark {
+        position: absolute;
+        height: 20px;
+        width: 20px;
+        opacity: 0;
+        transition: all 500ms;
+        transform: rotate(-30deg) scale(1.5);
+    }
+
+    .ani-checkbox-mark__left {
+        height: 2px;
+        width: 6px;
+        top: 12px;
+        left: 1px;
+        background-color: white;
+        position: absolute;
+        transform: rotate(45deg);
+    }
+    .ani-checkbox-mark__right {
+        height: 2px;
+        width: 16px;
+        top: 10px;
+        left: 4px;
+        background-color: white;
+        position: absolute;
+        transform: rotate(-45deg);
+    }
 </style>
