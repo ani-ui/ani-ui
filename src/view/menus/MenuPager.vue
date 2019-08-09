a81bcacf0e3bbfc13e90d3f9a90b2aa51dea0545
laiiihz <laiiihz@outlook.com>
2019-08-09 11:06:51 +0800

    [update] update docs

--- src/view/menus/MenuPager.vue
+++ src/view/menus/MenuPager.vue
@@ -1,8 +1,8 @@
 <template>
     <div style="width: 90%">
         <h1>ani-pager 分页器</h1>
-        <card-code-display>
-            <ani-pager :all-page="10" :current-page.sync="currentPage"></ani-pager>
+        <card-code-display title="基础使用" :code="codePager" :js="codePagerJs">
+            <ani-pager :all-page="50" :current-page.sync="currentPage"></ani-pager>
         </card-code-display>
     </div>
 </template>
@@ -16,6 +16,12 @@
         data(){
             return {
                 currentPage:1,
+                codePager:`<ani-pager :all-page="50" :current-page.sync="currentPage"></ani-pager>`,
+                codePagerJs:`data(){
+        return {
+            currentPage:1,
+        }
+    },`
             }
         },
     }
