6c826504da143767fa91a70400c14fb1d48aed79
laiiihz <laiiihz@outlook.com>
2019-08-09 15:53:28 +0800

    [update] update ani-pager style when press

--- src/components/AniPager.vue
+++ src/components/AniPager.vue
@@ -69,46 +69,41 @@ export default {
     allPage: Number,
     currentPage: Number
   },
-    mounted(){
-      if(this.allPage<=10){
-          this.rightMore=false
-      }
-    },
+  mounted() {
+    if (this.allPage <= 10) {
+      this.rightMore = false;
+    }
+  },
   computed: {
     firstArrow() {
       return this.currentPage !== 1;
     },
     lastArrow() {
-      return this.currentPage !== this.allPage
+      return this.currentPage !== this.allPage;
     },
     displayPage() {
       let array = [];
-      if(this.allPage<=10){
-          for (let i = 2; i < this.allPage; i++) {
-              array.push(i);
-          }
-      }
-      else if (this.currentPage < 5) {
+      if (this.allPage <= 10) {
+        for (let i = 2; i < this.allPage; i++) {
+          array.push(i);
+        }
+      } else if (this.currentPage < 5) {
         for (let i = 2; i < 10; i++) {
           array.push(i);
         }
-      }
-      else if(this.currentPage === 5){
-          for (let i = 2; i < 10; i++) {
-              array.push(i);
-          }
-      }
-      else if (this.currentPage > 5 &&this.currentPage < this.allPage - 5) {
+      } else if (this.currentPage === 5) {
+        for (let i = 2; i < 10; i++) {
+          array.push(i);
+        }
+      } else if (this.currentPage > 5 && this.currentPage < this.allPage - 5) {
         for (let i = this.currentPage - 3; i < this.currentPage + 4; i++) {
           array.push(i);
         }
-      }
-      else if(this.allPage - this.currentPage === 5){
-          for (let i = this.currentPage - 3; i < this.currentPage + 4; i++) {
-              array.push(i);
-          }
-      }
-      else if (this.allPage - this.currentPage < 5) {
+      } else if (this.allPage - this.currentPage === 5) {
+        for (let i = this.currentPage - 3; i < this.currentPage + 4; i++) {
+          array.push(i);
+        }
+      } else if (this.allPage - this.currentPage < 5) {
         for (let i = this.allPage - 8; i < this.allPage; i++) {
           array.push(i);
         }
@@ -118,13 +113,12 @@ export default {
   },
   watch: {
     currentPage(value) {
-        if(this.allPage>10){
-            this.leftMore = value > 5;
-            this.rightMore = value < this.allPage - 4;
-        }else{
-            this.rightMore=false
-        }
-
+      if (this.allPage > 10) {
+        this.leftMore = value > 5;
+        this.rightMore = value < this.allPage - 4;
+      } else {
+        this.rightMore = false;
+      }
     }
   },
   methods: {
@@ -166,6 +160,7 @@ export default {
   text-align: center;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   transition: all 500ms;
+  border-radius: 6px;
 }
 .ani-pager-li-style__active {
   height: 35px;
@@ -174,10 +169,17 @@ export default {
   position: relative;
   line-height: 35px;
   text-align: center;
+  border-radius: 6px;
   background-color: rgba(0, 0, 0, 0.5);
   color: white;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
   transition: all 500ms;
+}
+.ani-pager-li-style__active:hover {
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.6);
+}
+.ani-pager-li-style__active:active {
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
 }
 .ani-pager-li-style:hover {
   background-color: rgba(0, 0, 0, 0.3);
@@ -185,6 +187,8 @@ export default {
 }
 .ani-pager-li-style:active {
   background-color: rgba(0, 0, 0, 0.4);
+  border-radius: 2px;
+  color: white;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.4);
 }
 </style>
