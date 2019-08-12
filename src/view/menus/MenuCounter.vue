d23a7d84c9731a0126c26ae2d35a06acbde7cb93
laiiihz <laiiihz@outlook.com>
2019-08-12 11:03:31 +0800

    [update] update ani-counter doc

--- src/view/menus/MenuCounter.vue
+++ src/view/menus/MenuCounter.vue
@@ -1,26 +1,30 @@
 <template>
-    <div style="width: 90%">
-        <h1>ani-counter 计数器</h1>
-        <card-code-display title="基础使用">
-            <ani-counter :step="20" v-model="number"></ani-counter>
-        </card-code-display>
-    </div>
+  <div style="width: 90%">
+    <h1>ani-counter 计数器</h1>
+    <card-code-display title="基础使用" :code="code" :js="codeJs">
+      <ani-counter :step="20" v-model="number"></ani-counter>
+    </card-code-display>
+  </div>
 </template>
 
 <script>
-    import CardCodeDisplay from "../component/CardCodeDisplay";
-    import AniCounter from "../../components/AniCounter";
-    export default {
-        name: "MenuCounter",
-        components: {AniCounter, CardCodeDisplay},
-        data(){
-            return {
-                number:10,
-            }
-        },
-    }
+import CardCodeDisplay from "../component/CardCodeDisplay";
+import AniCounter from "../../components/AniCounter";
+export default {
+  name: "MenuCounter",
+  components: { AniCounter, CardCodeDisplay },
+  data() {
+    return {
+      number: 10,
+      code: ` <ani-counter :step="20" v-model="number"></ani-counter>`,
+      codeJs: `data(){
+        return {
+            number:10,
+        }
+    },`
+    };
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
