acff97c21ebf6084a11e89d27c082d2acc6259dd
laiiihz <laiiihz@outlook.com>
2019-08-13 10:24:04 +0800

    [add] add ani-tag component

---
+++ src/components/AniTag.vue
@@ -0,0 +1,106 @@
+<template>
+  <div
+    :class="[removable ? 'ani-tag-base-remove__style' : 'ani-tag-base__style']"
+  >
+    <span :class="[removable ? 'ani-tag-span__style' : '']">{{ tag }}</span>
+      <div v-if="removable" class="ani-tag-remove-mark__style">
+          <div class="ani-tag-close-icon__style close-tag-left"></div>
+          <div class="ani-tag-close-icon__style close-tag-right"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AniTag",
+  props: {
+    tag: String,
+    removable: Boolean
+  }
+};
+</script>
+
+<style scoped>
+.ani-tag-base__style {
+  height: 20px;
+  min-width: 40px;
+  display: inline-block;
+  padding: 4px 8px;
+  line-height: 20px;
+  border-radius: 5px;
+  transition: all 500ms;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+}
+.ani-tag-base__style:hover,
+.ani-tag-base-remove__style:hover {
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+}
+.ani-tag-base__style:active,
+.ani-tag-base-remove__style:active {
+  box-shadow: 0 0 0 rgba(0, 0, 0, 0.2);
+}
+.ani-tag-base-remove__style {
+  height: 20px;
+  min-width: 40px;
+  display: inline-block;
+  padding: 4px 0;
+  line-height: 20px;
+  border-radius: 5px;
+  transition: all 500ms;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+    position: relative;
+}
+
+.ani-tag-span__style {
+  padding-right: 18px;
+  padding-left: 18px;
+  transition: all 500ms;
+}
+.ani-tag-base-remove__style:hover .ani-tag-span__style {
+  padding-right: 27px;
+  padding-left: 9px;
+}
+
+.ani-tag-remove-mark__style {
+    height: 18px;
+    width: 18px;
+    position: absolute;
+    right: 18px;
+    top: 4px;
+    border-radius: 16px;
+    opacity: 0;
+    transition: all 500ms;
+}
+.ani-tag-base-remove__style:hover .ani-tag-remove-mark__style{
+    right: 6px;
+    opacity: 1;
+}
+.ani-tag-remove-mark__style:hover{
+    background-color: rgba(0,0,0,0.3);
+}
+.ani-tag-remove-mark__style:active{
+    background-color: rgba(0,0,0,0.5);
+}
+.ani-tag-remove-mark__style:active .ani-tag-close-icon__style{
+    background-color: rgba(255,255,255,1);
+}
+.ani-tag-close-icon__style {
+    width: 14px;
+    height: 2px;
+    transition: all 500ms;
+    position: absolute;
+    background-color: rgba(0,0,0,0.6);
+    top: 3px;
+}
+
+.close-tag-left {
+    left: 5px;
+    transform-origin: 0 0;
+    transform: rotate(45deg);
+}
+.close-tag-right {
+    right: 5px;
+    transform-origin: 14px 0;
+    transform: rotate(-45deg);
+}
+</style>
---
+++ src/view/menus/MenuTag.vue
@@ -0,0 +1,24 @@
+<template>
+    <div style="width: 90%">
+        <h1>ani-tag</h1>
+        <card-code-display title="基础使用">
+            <ani-tag tag="标签1"></ani-tag>
+        </card-code-display>
+        <card-code-display title="可移除">
+            <ani-tag tag="标签2" removable></ani-tag>
+        </card-code-display>
+    </div>
+</template>
+
+<script>
+    import CardCodeDisplay from "../component/CardCodeDisplay";
+    import AniTag from "../../components/AniTag";
+    export default {
+        name: "MenuTag",
+        components: {AniTag, CardCodeDisplay}
+    }
+</script>
+
+<style scoped>
+
+</style>
