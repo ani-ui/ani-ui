551f381c1f42b0d8dd2c0cefbfa75ada5f2ca77e
laiiihz <laiiihz@outlook.com>
2019-08-14 16:47:09 +0800

    feat : add simulator cloud using svg with div
    only one cloud could exist,need to fix it

--- src/view/menus/MenuCloud.vue
+++ src/view/menus/MenuCloud.vue
@@ -1,8 +1,12 @@
 <template>
     <div style="width: 90%">
         <h1>ani-cloud 模拟云</h1>
-        <card-code-display>
-            <ani-cloud></ani-cloud>
+        <card-code-display title="基础使用">
+           <div style="width: 100%;height: 270px;background-color:#76ebff;">
+               <div style="">
+                   <ani-cloud seed="10" animation></ani-cloud>
+               </div>
+           </div>
         </card-code-display>
     </div>
 </template>
